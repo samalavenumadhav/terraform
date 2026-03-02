443778152db34e7b20c81690d0c3b3ecd952ceab
venu madhav <madhavsamala171@gmail.com>
2026-03-02 13:14:24 +0530

    actions

---
+++ data-sources/data.tf
@@ -0,0 +1,22 @@
+data "aws_ami" "joindevops" {
+  most_recent = true
+  owners      = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+   filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+   filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+data "aws_instance" "terraform_instance"{
+    instance_id = "i-021f6144e95e880f6"
+}
